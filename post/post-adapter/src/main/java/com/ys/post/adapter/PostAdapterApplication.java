2c3489ff52e9960032bdf9e2730a561ca8ff447d
youngseok <dblys152@gmail.com>
2023-05-24 23:59:05 +0900

    프로젝트 설정 변경

--- post/post-adapter/src/main/java/com/ys/post/adapter/PostAdapterApplication.java
+++ post/post-adapter/src/main/java/com/ys/post/adapter/PostAdapterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PostAdapterApplication {
 
-    public static void maiin(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(PostAdapterApplication.class, args);
     }
 
